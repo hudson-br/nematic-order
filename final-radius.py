a36b7f977dc0b182a711bc0080e22e0a1ed86ae3
Hudson Borja da Rocha <hudson.borja-da-rocha@college-de-france.fr>
2022-05-04 12:19:19 +0200

    include final-radius.py

---
+++ final-radius.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import configreader
+
+
+path = '/Users/morpho/Desktop/data'
+os.chdir(path)
+cwd = os.getcwd()
+
+print(cwd)
+
+contractility = []
+radius = [] 
+time = []
+for elem in os.listdir(cwd):
+    if elem.startswith("run"):
+        print(elem)
+        f = np.genfromtxt(elem+ '/output/data.csv', delimiter=';', unpack=True)
+        radius = np.append(radius,f[2][-1])
+        C = configreader.Config()
+        config = C.read(elem+  "/config.conf")
+        contractility = np.append(contractility, float(config["parameters"]["contractility_strength"]))
+
+os.chdir(path)
+cwd = os.getcwd()
+plt.plot(contractility, radius,'.')
+plt.xlabel('Contractility $\zeta/\zeta_0$')
+plt.ylabel('Final furrow radius')
+plt.savefig('furrow_radius.pdf')
+plt.show()
